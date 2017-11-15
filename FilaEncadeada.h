7d2e610dbe3e6782355398dfcf4d6754bac11640
Eric-zambom <eric.zambom@outlook.com>
2017-11-14 22:12:08 -0200

    Loco

--- FilaEncadeada.h
+++ FilaEncadeada.h
@@ -6,22 +6,17 @@ typedef struct N{
 } node;
 
 typedef struct{
-  node *topo;
+  node *fim;
+  node *inicio;
 }Fila;
 
 void inicializaFila(Fila *p){
-  p->topo=NULL;
+  p->inicio=NULL;
+  p->fim=NULL;
 }
 
 int vazia(Fila *p){
-  if(p->topo==NULL){
-    return 1;
-  }
-  return 0;
-}
-
-int cheia(Fila *p){
-  if(p->topo>=10){
+  if(p->fim==NULL){
     return 1;
   }
   return 0;
@@ -29,10 +24,10 @@ int cheia(Fila *p){
 
 void inserir(Fila *p, int valor){
   node *no;
-  no=(node *)malloc(sizeof(node));
+  no = (node *)malloc(sizeof(node));
   no->valor=valor;
-  no->proximo=p->topo;
-  p->topo=no;
+  no->proximo=p->fim;
+  p->fim=no;
 }
 
 int remover(Fila *p){
@@ -41,14 +36,14 @@ int remover(Fila *p){
     return;
   }
   node *temp;
-  temp=p->topo;
-  p->topo=p->topo->proximo;
+  temp=p->inicio;
+  p->inicio=p->inicio->proximo;
   return temp->valor;
 }
 
 void imprimeFila(Fila *p){
   node *temp;
-  temp=p->topo;
+  temp=p->fim;
   while(temp!=NULL){
     printf("%d ", temp->valor);
     temp=temp->proximo;
